2d1dde92771dc4babeefd91ab5470a8e2304ded8
Lê Thành Đạt <lethanhdat1567@gmail.com>
2026-04-08 10:13:57 +0700

    fix migrate errorerror

--- prisma/migrations/20260317022752_update_message_tabe/migration.sql
+++ prisma/migrations/20260317022752_update_message_tabe/migration.sql
@@ -1,10 +1,10 @@
 /*
   Warnings:
 
-  - You are about to drop the column `isRead` on the `message` table. All the data in the column will be lost.
+  - You are about to drop the column `isRead` on the `Message` table. All the data in the column will be lost.
 
 */
 -- AlterTable
-ALTER TABLE `message` DROP COLUMN `isRead`,
+ALTER TABLE `Message` DROP COLUMN `isRead`,
     ADD COLUMN `readByAdmin` BOOLEAN NOT NULL DEFAULT false,
     ADD COLUMN `readByUser` BOOLEAN NOT NULL DEFAULT false;
